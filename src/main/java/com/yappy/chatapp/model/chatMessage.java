cc414090d5b56b19ea58b51ca3e5746802cf13ca
Sudheer <sudheerjillellamudi123@gmail.com>
2025-02-04 10:04:37 +0530

    created the model for chat message

--- src/main/java/com/yappy/chatapp/model/chatMessage.java
+++ src/main/java/com/yappy/chatapp/model/chatMessage.java
@@ -1,5 +1,7 @@
 package com.yappy.chatapp.model;
 
+import org.apache.logging.log4j.message.Message;
+
 public class chatMessage {
     private String sender;
     private String message;
@@ -10,6 +12,25 @@ public class chatMessage {
     }
 
     // getters and setters for the chatMessage object
-    
+    public String getSender() {
+        return sender;
+    }
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public MessageType getMessageType() {
+        return type;
+    }
+    public void setMessageType(MessageType type) {
+        this.type = type;
+    }
 
 }
