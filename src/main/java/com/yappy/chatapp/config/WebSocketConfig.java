c8ba9c4cafc9ecb3f84210738571acc90f473d97
Sudheer <sudheerjillellamudi123@gmail.com>
2025-03-02 19:50:49 +0530

    stomp now working in lan networks

--- src/main/java/com/yappy/chatapp/config/WebSocketConfig.java
+++ src/main/java/com/yappy/chatapp/config/WebSocketConfig.java
@@ -6,20 +6,27 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws")
+            .setAllowedOrigins(
+                "http://localhost:5173",      // Local development
+                "http://192.168.137.1:5173",  // Your IP address
+                "http://127.0.0.1:5173"       // Alternative localhost
+            )
+            .withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
         registry.enableSimpleBroker("/topic");
+        
+        // Optional: Set heartbeat intervals
+        registry.setPreservePublishOrder(true);
     }
-    
 }
